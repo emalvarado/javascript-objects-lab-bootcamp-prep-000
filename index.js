df1ba55a182552a646b2d9812c669d419947b1b1
ELIZABETH ALVARADO <lily21418@yahoo.com>
2018-09-02 02:29:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {chocolate: 1}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return object.assign({}, object, {[key]:value})
+  return Object.assign({}, object, {[key]:value})
 }
 
